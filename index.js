cf16b47926caaeae3a6fd72be16c0dc6fea7d643
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2024-10-06 16:31:32 -0300

    chore: finalizing the structuring

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const flash = require("express-flash");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+
+const app = express();
+
+const conn = require("./db/conn");
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+//session middleware
+app.use(
+  session({
+    name: "session",
+    secret: "nosso_secret",
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require("path").join(require("os").tmpdir(), "sessions"),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 3600000,
+      expires: new Date(Date.now() + 3600000),
+      httpOnly: true,
+    },
+  })
+);
+
+// flash messages
+app.use(flash());
+
+// public path
+app.use(express.static("public"));
+
+// set session to res
+app.use((req, res, next) => {
+  // console.log(req.session)
+  console.log(req.session.userid);
+
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
+
+  next();
+});
+
+conn
+  .sync()
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => console.log(error));
